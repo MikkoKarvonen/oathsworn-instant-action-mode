979c65971f13310d827f67ac819abfeb9349b388
MikkoKarvonen <mkarvonen34@gmail.com>
2024-10-06 12:31:00 +0300

    Add first chapter

--- src/components/Chapter.tsx
+++ src/components/Chapter.tsx
@@ -1,5 +1,22 @@
+import Chapter1 from "./chapter/Chapter1";
+// import Chapter2 from "./chapter/Chapter2";
+// import Chapter3 from "./chapter/Chapter3";
+
 const Chapter = ({ chapter }: { chapter: number }) => {
-  return <div>Chapter {chapter}</div>;
+  const chapters: { [key: number]: React.ComponentType } = {
+    1: Chapter1,
+    // 2: Chapter2,
+    // 3: Chapter3,
+  };
+
+  const SelectedChapter = chapters[chapter];
+
+  return (
+    <>
+      <h2>Chapter {chapter}</h2>
+      {SelectedChapter ? <SelectedChapter /> : <p>Chapter not found</p>}
+    </>
+  );
 };
 
 export default Chapter;
